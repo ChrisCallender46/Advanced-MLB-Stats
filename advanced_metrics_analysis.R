15e11e994a9bbe67ac354c862f77b67bb3877368
ChrisCallender46 <chriscallender46@gmail.com>
2025-04-10 17:28:22 -0400

    Add files via upload

---
+++ advanced_metrics_analysis.R
@@ -0,0 +1,44 @@
+library(tidyverse)
+
+# Exploratory Data Analysis (EDA)
+data <- WAR_vs_wRC_ |>
+  select(!Player) |>
+  filter(WAR_vs_wRC_$`Career wRC+` < 200)
+
+data <- data |>
+  mutate(
+    OPS = OBP + SLG
+  )
+
+View(cor(data))
+
+ggplot(data, aes(x = `Career wRC+`, y = `Career WAR`)) +
+  geom_point() +
+  geom_smooth()
+
+ggplot(data, aes(x = wOBA, y = `Career WAR`)) +
+  geom_point() +
+  geom_smooth()
+
+# ggplot(data, aes(x = OBP, y = `Career WAR`)) +
+#   geom_point() +
+#   geom_smooth()
+# 
+# ggplot(data, aes(x = OPS, y = `Career WAR`)) +
+#   geom_point() +
+#   geom_smooth()
+
+ggplot(data, aes(x = `OPS+`, y = `Career WAR`)) +
+  geom_point() +
+  geom_smooth()
+
+# We can see that the line of best fit has positive slope,
+# That means positive correlation
+
+data_norm <- scale(data)
+data_pca <- princomp(data_norm)
+summary(data_pca)
+
+data_pca$loadings[, 1:3]
+
+

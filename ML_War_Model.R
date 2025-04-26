d72b6104a89f7691a67177bdd067061770115189
Chris Callender <chriscallender46@gmail.com>
2025-04-26 17:02:58 -0400

    Add files via upload

--- ML_War_Model.R
+++ ML_War_Model.R
@@ -1,20 +1,39 @@
 # Build ML model to predict WAR
 
-model_data <- data |>
-  select(!`Career WAR`)
+library(e1071)
+library(caret)
 
-WAR_equation <- "`Career WAR` ~ ."
+model_data <- data |>
+  select(!`WAR`)
+
+WAR_equation <- "`WAR` ~ ."
 WAR_formula <- as.formula(WAR_equation)
 
-war_model <- lm(formula = WAR_formula, data = data, seed = 42)
+war_model <- svm(formula = WAR_formula, data = data, seed = 42)
 WAR <- predict(war_model, newdata = model_data)
 
 train <- WAR_vs_wRC_ |>
-  filter(`Career wRC+` < 200)
+  filter(`wRC+` < 200)
 
 player <- train$Player
 
 output_df <- as.data.frame(player)
 output_df$WAR <- WAR
 
-# Compare output_df to train dataset
+true_war <- WAR_vs_wRC_ |>
+  select(Player, `WAR`) |>
+  filter(WAR_vs_wRC_$`wRC+` < 200)
+
+# Calculate precision and recall
+ref_values <- train$`Career WAR`
+ref_values_fact <- as.factor(ref_values)
+
+WAR_values <- as.numeric(WAR)
+WAR_values_fact <- as.factor(WAR_values)
+
+precision <- posPredValue(WAR_values_fact, ref_values_fact, positive="1")
+recall <- sensitivity(WAR, true_war$`WAR`, positive="1")
+
+conf_matrix <- confusionMatrix(data = WAR_values_fact, reference = ref_values_fact)
+
+                               
